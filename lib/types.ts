291a5a8e37e1d96c4871723f6739d2a15ae8d66f
yosipy <todacchi.desu@gmail.com>
2024-07-28 04:22:22 +0900

    Improved lib/types

--- lib/types.ts
+++ lib/types.ts
@@ -1,13 +1,19 @@
 export type RejectedCode = "more-than-one-file" | "accept-violations"
 
+export type AcceptedClassifiedFile = {
+  status: "accepted"
+  file: File
+  errorCode: undefined
+}
+
+export type RejectedClassifiedFile<
+  CustonRejectedCode extends string = RejectedCode
+> = {
+  status: "rejected"
+  file: File
+  errorCode: RejectedCode | CustonRejectedCode
+}
+
 export type ClassifiedFile<CustonRejectedCode extends string = RejectedCode> =
-  | {
-      status: "accepted"
-      file: File
-      errorCode: undefined
-    }
-  | {
-      status: "rejected"
-      file: File
-      errorCode: RejectedCode | CustonRejectedCode
-    }
+  | AcceptedClassifiedFile
+  | RejectedClassifiedFile<CustonRejectedCode>
