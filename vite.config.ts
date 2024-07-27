1f5740265f0837c1b03f7df2f0003bf19e2b3823
yosipy <todacchi.desu@gmail.com>
2024-07-28 02:41:54 +0900

    Add lib alias

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc"
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
+    resolve: {
+      alias: [{ find: "@lib", replacement: "/lib" }],
+    },
     build: {
       outDir: "dist",
       lib: {
