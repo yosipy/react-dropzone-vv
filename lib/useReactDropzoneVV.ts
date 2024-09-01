23119e4ac333f583a64816a0ebce98b7079c25ab
yosipy <todacchi.desu@gmail.com>
2024-09-01 21:56:31 +0900

    feat: add DisableOutOfFileDropzoneZone and removed disabledDropOnDocment

--- lib/useReactDropzoneVV.ts
+++ lib/useReactDropzoneVV.ts
@@ -3,7 +3,6 @@ import { useRef, useState } from "react"
 export type UseReactDropzoneVVProps = {
   accept?: string
   disabled?: boolean
-  disabledDropOnDocment?: boolean
   multiple?: boolean
 }
 
@@ -12,8 +11,6 @@ export type UseReactDropzoneVV = {
   setAccept: React.Dispatch<React.SetStateAction<string>>
   disabled: boolean
   setDisabled: React.Dispatch<React.SetStateAction<boolean>>
-  disabledDropOnDocment: boolean
-  setDisabledDropOnDocment: React.Dispatch<React.SetStateAction<boolean>>
   multiple: boolean
   setMultiple: React.Dispatch<React.SetStateAction<boolean>>
   isDragging: boolean
@@ -25,14 +22,10 @@ export type UseReactDropzoneVV = {
 export const useReactDropzoneVV = ({
   accept: tAccept = "",
   disabled: tDisabled = false,
-  disabledDropOnDocment: tDisabledDropOnDocment = false,
   multiple: tMultiple = true,
 }: UseReactDropzoneVVProps = {}): UseReactDropzoneVV => {
   const [accept, setAccept] = useState<string>(tAccept)
   const [disabled, setDisabled] = useState<boolean>(tDisabled)
-  const [disabledDropOnDocment, setDisabledDropOnDocment] = useState<boolean>(
-    tDisabledDropOnDocment
-  )
   const [multiple, setMultiple] = useState<boolean>(tMultiple)
 
   const [isDragging, setIsDragging] = useState(false)
@@ -48,8 +41,6 @@ export const useReactDropzoneVV = ({
     setAccept,
     disabled,
     setDisabled,
-    disabledDropOnDocment,
-    setDisabledDropOnDocment,
     multiple,
     setMultiple,
     isDragging,
