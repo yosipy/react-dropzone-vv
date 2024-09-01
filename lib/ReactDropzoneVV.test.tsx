8e2d21fe11001f845659ed25d7a765087d2b253a
yosipy <todacchi.desu@gmail.com>
2024-09-02 04:33:04 +0900

    feat: add component test

---
+++ lib/ReactDropzoneVV.test.tsx
@@ -0,0 +1,95 @@
+import { afterEach, beforeEach, describe, expect, test } from "vitest"
+import { render, fireEvent, act, cleanup } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { FC } from "react"
+import { OnSelectProps, ReactDropzoneVV } from "./ReactDropzoneVV"
+import { useReactDropzoneVV } from "./useReactDropzoneVV"
+import { RejectedClassifiedFile } from "./types"
+
+let acceptedFiles: File[]
+let fileRejections: RejectedClassifiedFile[]
+
+// Mock a file
+const file = new File(["dummy content"], "example.png", { type: "image/png" })
+
+type Props = {
+  accept?: string
+  disabled?: boolean
+  multiple?: boolean
+}
+
+const TestComponent: FC<Props> = (props) => {
+  const reactDropzoneVV = useReactDropzoneVV()
+
+  const handleSelect = (props: OnSelectProps) => {
+    console.log("handleSelect")
+    acceptedFiles = props.acceptedFiles
+    fileRejections = props.fileRejections
+  }
+
+  return (
+    <ReactDropzoneVV
+      reactDropzoneVV={reactDropzoneVV}
+      accept={props.accept}
+      disabled={props.disabled}
+      multiple={props.multiple}
+      onSelect={handleSelect}
+    >
+      dropzone
+    </ReactDropzoneVV>
+  )
+}
+
+describe("ReactDropzoneVV", () => {
+  afterEach(cleanup)
+
+  describe("accept", () => {
+    test("sets 'accept' props in <input>", async () => {
+      const { container } = render(<TestComponent accept=".png" />)
+
+      expect(container.querySelector("input")).toHaveProperty("accept", ".png")
+    })
+
+    describe("inputed file type includes accept", () => {
+      test("acceptedFiles includes files", async () => {
+        const { getByText } = render(<TestComponent />)
+        const dropzone = getByText(/dropzone/i)
+
+        fireEvent.drop(dropzone, {
+          dataTransfer: { files: [file] },
+        })
+
+        expect(acceptedFiles).toStrictEqual([file])
+      })
+    })
+
+    describe("inputed file type excludes accept", () => {
+      test("acceptedFiles is empty array", async () => {
+        const { getByText } = render(<TestComponent accept=".json" />)
+        const dropzone = getByText(/dropzone/i)
+
+        fireEvent.drop(dropzone, {
+          dataTransfer: { files: [file] },
+        })
+
+        expect(acceptedFiles).toStrictEqual([])
+      })
+    })
+  })
+
+  describe("disabled", () => {
+    test("sets 'disabled' props in <input>", async () => {
+      const { container } = render(<TestComponent disabled={true} />)
+
+      expect(container.querySelector("input")).toHaveProperty("disabled", true)
+    })
+  })
+
+  describe("multiple", () => {
+    test("sets 'multiple' props in <input>", async () => {
+      const { container } = render(<TestComponent multiple={false} />)
+
+      expect(container.querySelector("input")).toHaveProperty("multiple", false)
+    })
+  })
+})
