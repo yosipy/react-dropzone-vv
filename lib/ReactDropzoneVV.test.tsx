3a2b3d530a0d065ceace32cf482afa976b0ba5c8
yosipy <todacchi.desu@gmail.com>
2024-09-02 05:09:07 +0900

    feat: add noClick & noDrag

--- lib/ReactDropzoneVV.test.tsx
+++ lib/ReactDropzoneVV.test.tsx
@@ -14,6 +14,8 @@ type Props = {
   accept?: string
   disabled?: boolean
   multiple?: boolean
+  noClick?: boolean
+  noDrag?: boolean
 }
 
 const TestComponent: FC<Props> = (props) => {
@@ -31,6 +33,8 @@ const TestComponent: FC<Props> = (props) => {
       accept={props.accept}
       disabled={props.disabled}
       multiple={props.multiple}
+      noClick={props.noClick}
+      noDrag={props.noDrag}
       onSelect={handleSelect}
     >
       dropzone
@@ -39,7 +43,12 @@ const TestComponent: FC<Props> = (props) => {
 }
 
 describe("ReactDropzoneVV", () => {
-  afterEach(cleanup)
+  afterEach(() => {
+    cleanup()
+
+    acceptedFiles = []
+    fileRejections = []
+  })
 
   describe("accept", () => {
     test("sets 'accept' props in <input>", async () => {
@@ -95,6 +104,65 @@ describe("ReactDropzoneVV", () => {
       const { container } = render(<TestComponent multiple={false} />)
 
       expect(container.querySelector("input")).toHaveProperty("multiple", false)
+    })
+  })
+
+  describe("noClick", () => {
+    const example = (noClick: boolean) => {
+      const { container } = render(<TestComponent noClick={noClick} />)
+      const inputElem = container.querySelector("input")
+
+      if (!inputElem) throw new Error("inputElem is null")
+      fireEvent.change(inputElem, {
+        target: { files: [file] },
+      })
+    }
+
+    describe("noClick is true", () => {
+      test("acceptedFiles and fileRejections is empty array", async () => {
+        example(true)
+
+        expect(acceptedFiles.length).toBe(0)
+        expect(fileRejections.length).toBe(0)
+      })
+    })
+
+    describe("noClick is false", () => {
+      test("acceptedFiles or fileRejections is non-empty array", async () => {
+        example(false)
+
+        expect(acceptedFiles.length).toBe(1)
+        expect(fileRejections.length).toBe(0)
+      })
+    })
+  })
+
+  describe("noDrag", () => {
+    const example = (noDrag: boolean) => {
+      const { getByText } = render(<TestComponent noDrag={noDrag} />)
+      const dropzone = getByText(/dropzone/i)
+
+      fireEvent.drop(dropzone, {
+        dataTransfer: { files: [file] },
+      })
+    }
+
+    describe("noDrag is true", () => {
+      test("acceptedFiles and fileRejections is empty array", async () => {
+        example(true)
+
+        expect(acceptedFiles.length).toBe(0)
+        expect(fileRejections.length).toBe(0)
+      })
+    })
+
+    describe("noDrag is false", () => {
+      test("acceptedFiles or fileRejections is non-empty array", async () => {
+        example(false)
+
+        expect(acceptedFiles.length).toBe(1)
+        expect(fileRejections.length).toBe(0)
+      })
     })
   })
 })
